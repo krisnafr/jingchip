270dffd518c1ec139f63544350a234bb369d5020
Krisna <krisnafawwastaqi@gmail.com>
2018-04-29 17:15:20 +0700

    welcome page

---
+++ application/views/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, maximum-scale=1">
+
+        <title>Welcome - JING, jagung emping dengan varian rasa</title>
+        <link rel="icon" href="assets/favicon.png" type="image/png">
+        <link rel="shortcut icon" href="assets/favicon.ico" type="img/x-icon">
+
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800italic,700italic,600italic,400italic,300italic,800,700,600' rel='stylesheet' type='text/css'>
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/animate.css" rel="stylesheet" type="text/css">
+
+        <!-- =======================================================
+    Theme Name: Knight
+    Theme URL: https://bootstrapmade.com/knight-free-bootstrap-theme/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+        ======================================================= -->
+
+    </head>
+
+    <body>
+        <header class="header" id="header">
+            <!--header-start-->
+            <div class="container">
+                <h1 class="animated fadeInDown delay-07s">WELCOME TO JING CHIPS</h1>
+                <figure class="logo animated fadeInDown delay-07s">
+                    <a href="#"><img src="assets/img/logo.png" alt=""></a>
+                </figure>
+
+                <ul class="we-create animated fadeInUp delay-1s">
+                    <li>Enak banget jing! cobain deh!</li>
+                </ul>
+                <a class="link animated fadeInUp delay-1s servicelink" href="#service">cobain</a>
+            </div>
+        </header>
+        <!--header-end-->
+        <footer class="footer">
+            <div class="container">
+<!--			<div class="footer-logo"><a href="#"><img src="assets/img/footer-logo.png" alt=""></a></div>
+                    <span class="copyright">&copy; Knight Theme. All Rights Reserved</span>
+                    <div class="credits">-->
+                <!--
+All the links in the footer should remain intact.
+You can delete the links only if you purchased the pro version.
+Licensing information: https://bootstrapmade.com/license/
+Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Knight
+                -->
+                <a href="https://bootstrapmade.com/bootstrap-agency-templates/">Bootstrap Agency Templates</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
